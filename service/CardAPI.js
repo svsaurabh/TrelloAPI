ab638d879f21f9b42851e172bd8fea32052cd8ce
svsaurabh <svsaurabh3232@gmail.com>
2020-07-20 22:28:34 +0530

    Added req data

--- service/CardAPI.js
+++ service/CardAPI.js
@@ -1,10 +1,9 @@
 const trelloNode = require('trello-node-api')('60b27d31e9f987aa774cfef8a80d0bc5', '824cec21a48489fd2c31a20383ddcc687537e5bf2eed57785be4f207426da398');
-//const say = require('say')
 
 exports.createCard = (req, res) => {
     var data = {
-        name: 'Card via API',
-        desc: 'Card description',
+        name: req.body.name,
+        desc: req.body.description,
         pos: 'top',
         idList: '5f11a3b3c2e3ec73f963348b',
     };
